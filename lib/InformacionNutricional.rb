61a9e42c74ca2718c49969961bbe70f81157e78e
Ana Cáceres <alu0100972016@ull.edu.es>
2018-11-06 08:41:59 +0000

    Creación de la gema InformacionNutricional

---
+++ lib/InformacionNutricional.rb
@@ -0,0 +1,6 @@
+require "InformacionNutricional/version"
+
+module InformacionNutricional
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/InformacionNutricional/version.rb
@@ -0,0 +1,3 @@
+module InformacionNutricional
+  VERSION = "0.1.0"
+end
