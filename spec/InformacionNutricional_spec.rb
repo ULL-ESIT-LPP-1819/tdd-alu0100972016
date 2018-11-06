a7305d92b90b5f160ee050d4522b2b07d711bedb
Ana Cáceres <alu0100972016@ull.edu.es>
2018-11-06 13:12:35 +0000

    Añadir los valores nutricionales y su prueba

--- spec/InformacionNutricional_spec.rb
+++ spec/InformacionNutricional_spec.rb
@@ -1,9 +1,45 @@
+require 'InformacionNutricional/etiqueta.rb'
+
 RSpec.describe InformacionNutricional do
+ 
+  before :all do
+	  @pan = Etiqueta.new("Pan", true, 28.5, 15.0, 3.8, 1.8, 38.0, 2.5, 3.8, 0.4)
+  end
+
   it "has a version number" do
     expect(InformacionNutricional::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "has a name" do
+	  expect(@pan.name).to eq("Pan")
   end
+
+  it "has portions" do
+	  expect(@pan.portions).to eq(true)
+  end
+
+  it "has fats" do
+	  expect(@pan.fats).to eq(3.8)
+  end
+
+  it "has s_fats" do
+	  expect(@pan.s_fats).to eq(1.8)
+  end
+
+  it "has carbs" do
+	  expect(@pan.carbs).to eq(38.0)
+  end
+
+  it "has sugar" do
+	  expect(@pan.sugar).to eq(2.5)
+  end
+
+  it "has protein" do
+	  expect(@pan.protein).to eq(3.8)
+  end
+
+  it "has salt" do
+	  expect(@pan.salt).to eq(0.4)
+  end
+
 end
