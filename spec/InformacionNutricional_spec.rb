907e8b564283369e2b851de644a6dc19599590a4
Ana Cáceres <alu0100972016@ull.edu.es>
2018-11-19 18:29:41 +0000

    Añadiendo pruebas de clase, tipo y jerarquía

--- spec/InformacionNutricional_spec.rb
+++ spec/InformacionNutricional_spec.rb
@@ -20,46 +20,48 @@ RSpec.describe InformacionNutricional do
     expect(InformacionNutricional::VERSION).not_to be nil
   end
 
-  it "has a name" do
-	  expect(@pan.name).to eq("Pan")
-  end
+  describe "#Etiqueta" do
+    it "has a name" do
+        expect(@pan.name).to eq("Pan")
+    end
 
-  it "has portions" do
-	  expect(@pan.portions).to eq(true)
-  end
+    it "has portions" do
+        expect(@pan.portions).to eq(true)
+    end
 
-  it "has fats" do
-	  expect(@pan.fats).to eq(3.8)
-  end
+    it "has fats" do
+        expect(@pan.fats).to eq(3.8)
+    end
 
-  it "has s_fats" do
-	  expect(@pan.s_fats).to eq(1.8)
-  end
+    it "has s_fats" do
+        expect(@pan.s_fats).to eq(1.8)
+    end
 
-  it "has carbs" do
-	  expect(@pan.carbs).to eq(38.0)
-  end
+    it "has carbs" do
+        expect(@pan.carbs).to eq(38.0)
+    end
 
-  it "has sugar" do
-	  expect(@pan.sugar).to eq(2.5)
-  end
+    it "has sugar" do
+        expect(@pan.sugar).to eq(2.5)
+    end
 
-  it "has protein" do
-	  expect(@pan.protein).to eq(3.8)
-  end
+    it "has protein" do
+        expect(@pan.protein).to eq(3.8)
+    end
 
-  it "has salt" do
-	  expect(@pan.salt).to eq(0.4)
-  end
+    it "has salt" do
+        expect(@pan.salt).to eq(0.4)
+    end
 
-  it "has calories" do
-	  @pan.calculate_calories
-	  expect(@pan.kcal).to eq(203.8)
-	  expect(@pan.kj).to eq(862.4)
-  end
+    it "has calories" do
+        @pan.calculate_calories
+        expect(@pan.kcal).to eq(203.8)
+        expect(@pan.kj).to eq(862.4)
+    end
 
-  it "has format" do
-	  expect(@pan.to_s).to eq("PAN(28 porciones; 15.0g/porcion): Por 100g - IR; VALOR ENERGETICO: 203.8 kcal/862.4 kJ - 2000 kcal/8400 kJ; GRASAS: 3.8g - 70g; SATURADAS: 1.8g - 20g; CARBOHIDRATOS: 38.0g - 260g; AZUCARES: 2.5g - 90g; PROTEINAS: 3.8g - 50g; SAL: 0.4g - 6g;")
+    it "has format" do
+        expect(@pan.to_s).to eq("PAN(28 porciones; 15.0g/porcion): Por 100g - IR; VALOR ENERGETICO: 203.8 kcal/862.4 kJ - 2000 kcal/8400 kJ; GRASAS: 3.8g - 70g; SATURADAS: 1.8g - 20g; CARBOHIDRATOS: 38.0g - 260g; AZUCARES: 2.5g - 90g; PROTEINAS: 3.8g - 50g; SAL: 0.4g - 6g;")
+    end
   end
 
   describe "#List" do
@@ -160,5 +162,25 @@ RSpec.describe InformacionNutricional do
     it "has format" do
         expect(@p1.to_s).to eq("Nombre: María, Edad: 18, Sexo: Mujer")
     end      
+  end
+  
+  describe "Clase, tipo y pertenencia a jerarquía" do
+      it "Clase de un objeto" do
+          expect(@pan.instance_of? Etiqueta).to eq(true)
+          expect(@mylist.instance_of? List).to eq(true)
+          expect(@p1.instance_of? Persona).to eq(true)
+      end
+      
+      it "Tipo de un objeto" do
+          expect(@pan.respond_to?:name).to eq(true)
+          expect(@mylist.respond_to?:head).to eq(true)
+          expect(@p1.respond_to?:nombre).to eq(true)
+      end
+      
+      it "Jerarquía" do
+          expect(@pan.is_a? Object).to eq(true)
+          expect(@mylist.is_a? Object).to eq(true)
+          expect(@p1.is_a? Object).to eq(true)
+      end   
   end
 end
